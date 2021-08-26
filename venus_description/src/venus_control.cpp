c14bdf0ed28c18388278cd2fc40a0fd4ed7c8a4f
Yulei Qiu <vsper2016@gmail.com>
2021-08-26 10:29:15 +0000

    Initial structure of the control cpp file. No content, but can compile now

---
+++ venus_description/src/venus_control.cpp
@@ -0,0 +1,17 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+#include <queue>
+#include <vector>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "actuatorcontroller.h"
+
+using namespace std::chrono_literals;
+
+int main()
+{
+    return 0;
+}
