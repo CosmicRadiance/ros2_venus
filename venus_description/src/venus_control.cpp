a8b7120d24ca307f4ec50bbedd2ba5e766670769
Yulei Qiu <vsper2016@gmail.com>
2021-08-31 09:47:32 +0000

    Now the manipulator can be controlled using the slider in joint_state_publisher_gui

--- venus_description/src/venus_control.cpp
+++ venus_description/src/venus_control.cpp
@@ -1,5 +1,6 @@
+#include <iostream>
 #include <chrono>
-#include <functional>
+// #include <functional>
 #include <memory>
 #include <string>
 #include <queue>
@@ -19,7 +20,7 @@
 #define RAD_TO_POS(x) ((x / (2 * M_PI)) * STEERING_GEAR_RATIO)
 #define POS_TO_RAD(x) ((x * (2 * M_PI)) / STEERING_GEAR_RATIO)
 
-using std::placeholders::_1;
+// using std::placeholders::_1;
 
 static std::queue<sensor_msgs::msg::JointState> s_jointStatesList;
 
@@ -50,14 +51,14 @@ static std::queue<sensor_msgs::msg::JointState> s_jointStatesList;
 
 void jointStatesCallback(const sensor_msgs::msg::JointState::SharedPtr msg)
 {
-  if (msg == NULL) return;
-  s_jointStatesList.push(*msg);
-  if (s_jointStatesList.size() >= 10)
-  {
-    s_jointStatesList.pop();
-    RCLCPP_WARN(rclcpp::get_logger("callback"), "jointStatesCallback, pop");
-  }
-  RCLCPP_INFO(rclcpp::get_logger("callback"), "I got a message!");
+    if (msg == NULL) return;
+    s_jointStatesList.push(*msg);
+    if (s_jointStatesList.size() >= 10)
+    {
+        s_jointStatesList.pop();
+        RCLCPP_WARN(rclcpp::get_logger("callback"), "jointStatesCallback, pop");
+    }
+    // RCLCPP_INFO(rclcpp::get_logger("callback"), "I got a message!");
 }
 
 int main(int argc, char *argv[])
@@ -70,30 +71,81 @@ int main(int argc, char *argv[])
     auto node = rclcpp::Node::make_shared("joint_subscriber");
     auto subscription = node->create_subscription<sensor_msgs::msg::JointState>(
     "joint_states", 100, jointStatesCallback);
-    rclcpp::spin(node);
-    rclcpp::shutdown();
+    // rclcpp::spin(node);
+    // rclcpp::shutdown();
+
+    // Initialize the controller
+    ActuatorController* pController = ActuatorController::initController();
+    // ec Define an error type, ec==0x00 means no error,
+    // ec will be passed to pcontroller-> lookupActuators(ec) by reference,
+    // when the error occurs, ec value will be modified by SDK to the corresponding error code
+    Actuator::ErrorsDefine ec;
+    // Find the connected actuators and return the UnifiedID of all actuators found.
+    std::vector<ActuatorController::UnifiedID> uIDArray = pController->lookupActuators(ec);
+    // If the size of the uIDArray is greater than zero, the connected actuators have been found
+    if (uIDArray.size() > 0)
+    {
+        for(size_t k = 0; k < uIDArray.size(); k++)
+        {
+            ActuatorController::UnifiedID actuator = uIDArray.at(k);
+            // Enable actuator
+            RCLCPP_INFO(node->get_logger(), "actuator ID %d, ipAddr %s",
+            actuator.actuatorID,actuator.ipAddress.c_str());
+            pController->enableActuator(actuator.actuatorID,actuator.ipAddress);
+            // activate profile position mode
+            pController->activateActuatorMode(actuator.actuatorID,Actuator::Mode_Profile_Pos);
+
+            std::cout << "set position to zero positions " << std::endl;
+            pController->setPosition(actuator.actuatorID, 0);
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+    }
+    else
+    {
+        std::cout << "Connected error code:" << hex << ec << std::endl;
+        return -1;
+    }
+
+    RCLCPP_INFO(node->get_logger(), "Initialized!");
     
-    // rclcpp::Rate loop_rate(10);.
-    // while (rclcpp::ok())
-    // {
-    //   rclcpp::spin_some(node);
-    //   loop_rate.sleep();
-    //   if (!s_jointStatesList.empty())
-    //   {
-    //     sensor_msgs::msg::JointState::SharedPtr js_data;
-    //     *js_data = s_jointStatesList.front();
-    //     s_jointStatesList.pop();
-    //     for(int i = 0; i < 6; i++)
-    //     {
-    //       // Since we have 2 actuators at joint 2 (the index here is the same as that in Rviz 2),
-    //       // you have to deal with actuator No.2 and No.3,
-    //       // making them rotating to opposite directions
-    //       ActuatorController::UnifiedID actuator = uIDArray.at(i);
-    //       RCLCPP_INFO(node->get_logger(), "jointstate_%d, %s, %f", i+1, js_data->name[i].c_str(), js_data->position[i]);
-    //       pController->setPosition(actuator.actuatorID, RAD_TO_POS(js_data->position[i]));
-    //     }
-    //   }
-    // }
+    rclcpp::Rate loop_rate(50);
+    while (rclcpp::ok())
+    {
+        rclcpp::spin_some(node);
+        if (!s_jointStatesList.empty())
+        {
+            sensor_msgs::msg::JointState js_data;
+            js_data = s_jointStatesList.front();
+            s_jointStatesList.pop();
+            for(int i = 0; i < 6; i++)
+            {
+                // Since we have 2 actuators at joint 2 (the index here is the same as that in Rviz 2),
+                // you have to deal with actuator No.2 and No.3,
+                // making them rotating to opposite directions
+                ActuatorController::UnifiedID actuator = uIDArray.at(i);
+                // Actual movement at joint 1, 3, 4 are opposite
+                if (i < 2)
+                {
+                    RCLCPP_INFO(node->get_logger(),
+                    "jointstate_%d, %s, %f", i+1, js_data.name[i].c_str(), js_data.position[i]);
+                    pController->setPosition(actuator.actuatorID, RAD_TO_POS(js_data.position[i]));
+                }
+                else if (i == 2)
+                {
+                    RCLCPP_INFO(node->get_logger(),
+                    "jointstate_%d, %s, %f", i, js_data.name[i-1].c_str(), js_data.position[i-1]);
+                    pController->setPosition(actuator.actuatorID, -1*RAD_TO_POS(js_data.position[i-1]));
+                }
+                else
+                {
+                    RCLCPP_INFO(node->get_logger(),
+                    "jointstate_%d, %s, %f", i, js_data.name[i-1].c_str(), js_data.position[i-1]);
+                    pController->setPosition(actuator.actuatorID, RAD_TO_POS(js_data.position[i-1]));
+                }
+            }
+        }
+        loop_rate.sleep();
+    }
 
     return 0;
 }
